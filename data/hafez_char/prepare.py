a0333e33a2b3c59138583d486a7226adc7319b1d
Ali Ghorbani <ahorjia@users.noreply.github.com>
2023-05-07 17:28:48 -0700

    Add more output samples

--- data/hafez_char/prepare.py
+++ data/hafez_char/prepare.py
@@ -1,5 +1,5 @@
 """
-Prepare the Shakespeare dataset for character-level language modeling.
+Prepare the dataset for character-level language modeling.
 So instead of encoding with GPT-2 BPE tokens, we just map characters to ints.
 Will save train.bin, val.bin containing the ids, and meta.pkl containing the
 encoder and decoder and some other related info.
@@ -9,12 +9,12 @@ import pickle
 import requests
 import numpy as np
 
-# download the tiny shakespeare dataset
+# download the hafez ghazal dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-# if not os.path.exists(input_file_path):
-#     data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-#     with open(input_file_path, 'w') as f:
-#         f.write(requests.get(data_url).text)
+if not os.path.exists(input_file_path):
+    data_url = 'https://www.cs.cmu.edu/~nsharifr/hafez.txt'
+    with open(input_file_path, 'w') as f:
+        f.write(requests.get(data_url).text)
 
 with open(input_file_path, 'r', encoding="utf-8") as f:
     data = f.read()
@@ -60,9 +60,9 @@ meta = {
 with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
     pickle.dump(meta, f)
 
-# length of dataset in characters:  1115394
+# length of dataset in characters: 282,651
 # all the unique characters:
-#  !$&',-.3:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
-# vocab size: 65
-# train has 1003854 tokens
-# val has 111540 tokens
+#  Yآابتثجحخدذرزسشصضطظعغفقلمنهويپچژکگی۰۱۲۳۴۵۶۷۸۹‌
+# vocab size: 48
+# train has 254,385 tokens
+# val has 28,266 tokens
